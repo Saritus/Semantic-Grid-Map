02df85a763ee6655a674f5620f1ae3b911b296be
Sebastian Mischke <SebastianMischke@gmx.de>
2017-03-03 23:02:56 +0100

    Use random map as input

--- dt_with_dl.py
+++ dt_with_dl.py
@@ -7,31 +7,34 @@ from nolearn.lasagne import NeuralNet
 
 import matplotlib.pyplot as pyplot
 import numpy as np
+from scipy import ndimage
 
+PATCHSIZE = 11
 
 def load_data():
-    x_train = np.array([[0, 0], [1, 0], [0, 1], [1, 1], [0, 0], [1, 0], [0, 1], [1, 1]], dtype=np.float32)
-    y_train = np.array([0., 0., 0., 1., 0., 0., 0., 1.], dtype=np.float32)
+    x_train = []
+    y_train = []
 
-    x_valid = np.array([[0, 1], [1, 1], [0, 0], [1, 0]], dtype=np.float32)
-    y_valid = np.array([0., 1., 0., 0.], dtype=np.float32)
+    for x in np.arange(0, 20):
+        # Erstellen einer Zufallskarte
+        x_train_sample = np.random.randint(2, size=(11, 11))
+        y_train_sample = ndimage.distance_transform_edt(x_train_sample)
+        y_train_sample = 1
 
-    # Erstellen zweier leerer Arrays
-    x_test = []
-    y_test = []
+        #show_array(x_train_sample.reshape((PATCHSIZE, PATCHSIZE)))
+        #show_array(y_train_sample.reshape((PATCHSIZE, PATCHSIZE)))
 
-    # Fuellen der Arrays mit den entsprechenden Werten
-    for x in np.arange(0, 1.001, 0.01):
-        for y in np.arange(0, 1.001, 0.01):
-            x_test.extend([[x, y]])
-            y_test.extend([1 if (x-0.5)*(x-0.5)+(y-0.5)*(y-0.5)<0.1 else 0]) # 1, wenn x und y groesser 0.5, ansonsten 0
+        x_train.extend([[x_train_sample]])
+        y_train.extend([y_train_sample])
 
     # Umwandeln der beiden Arrays in numpy-Arrays
-    x_test = np.array(x_test, dtype=np.float32)
-    y_test = np.array(y_test, dtype=np.float32)
+    x_train = np.array(x_train, dtype=np.float32)
+    y_train = np.array(y_train, dtype=np.float32)
 
-    #x_test = np.array([[0.5, 0.5], [0.8, 0], [0.9, 0.9], [0, 0.8]], dtype=np.float32)
-    #y_test = np.array([0, 0, 1, 0], dtype=np.float32)
+    x_valid = x_train
+    y_valid = y_train
+    x_test = x_train
+    y_test = y_train
 
     return x_train, y_train, x_valid, y_valid, x_test, y_test
 
@@ -51,14 +54,14 @@ def create_net():
                 ('output', layers.DenseLayer),
                 ],
         # layer parameters:
-        input_shape=(None, 2),
+        input_shape=(None, 1, PATCHSIZE, PATCHSIZE),
 
-        hidden1_num_units=8,  # number of units in 'hidden' layer
+        hidden1_num_units=100,  # number of units in 'hidden' layer
         #hidden2_num_units=5,  # number of units in 'hidden' layer
         #hidden3_num_units=5,  # number of units in 'hidden' layer
         #hidden4_num_units=5,  # number of units in 'hidden' layer
 
-        output_num_units = 1,
+        output_num_units = 1,#PATCHSIZE*PATCHSIZE,
         output_nonlinearity = lasagne.nonlinearities.sigmoid,
 
         # optimization method:
@@ -67,8 +70,8 @@ def create_net():
         update_momentum=0.9,
 
         regression=True,
-        max_epochs=1000,
-        verbose=2,
+        max_epochs=20,
+        verbose=1,
     )
     return net1
 
@@ -82,11 +85,11 @@ def main():
     net1 = create_net()
 
     # Train the network
-    net1.fit(x_test, y_test)
+    net1.fit(x_train, y_train)
 
     # Show the result that we want and the result that we get
-    show_array(y_test.reshape((101, 101)))
-    show_array(net1.predict(x_test).reshape((101, 101)))
+    #show_array(y_test[0].reshape((PATCHSIZE, PATCHSIZE)))
+    #show_array(net1.predict(x_test[0,0]).reshape((PATCHSIZE, PATCHSIZE)))
 
     # Try the network on new data
     print("Label:\n%s" % str(y_test))
